263ec4fdead5f27b3f51632ed6b55662df0828b9
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 14:19:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(names)
+  
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  assign_rooms.each do |room|
+  puts "#{room}!"
+end
+end
