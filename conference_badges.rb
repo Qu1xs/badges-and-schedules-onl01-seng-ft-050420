ef917158d4354d2c84de9f3b67f6e780c7d51727
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 18:22:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,7 @@ end
 def batch_badge_creator(attendees)
   ary = []
   attendees.each { |name| ary.push("Hello, my name is #{name}.") }
+  puts ary
   return ary
 end
 
@@ -15,10 +16,11 @@ def assign_rooms(attendees)
   ary = []
   rooms = [1, 2, 3, 4, 5, 6, 7]
   attendees.each_with_index {|name, i| ary.push("Hello, #{name}! You'll be assigned to room #{rooms[i]}!")}
+  puts ary
   return ary
 end
 
 def printer(attendees)
- puts batch_badge_creator(attendees)
- puts assign_rooms(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
 end
